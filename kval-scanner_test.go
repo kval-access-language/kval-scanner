6c75cf7774820e65498f5820bbb130b5498efb95
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-11-21 12:43:33 +1300

    [refactor] Gofmt.

--- kval-scanner_test.go
+++ kval-scanner_test.go
@@ -1,51 +1,53 @@
 package kvalscanner
 
 import (
-	"strings"
-	"testing"
+   "strings"
+   "testing"
 )
 
+// Scanner test results struct
 type scannerTest struct {
-	scanvalue string
-	expected  Token
+   scanvalue string
+   expected  Token
 }
 
+// Scan basic Tokens
 var scannerTests = []scannerTest{
-	{"", EOF},
-	{"*", LITERAL},
-	{" ", WS},
-	{"_", USCORE},
-	{"\x1F", ILLEGAL},
+   {"", EOF},
+   {"*", LITERAL},
+   {" ", WS},
+   {"_", USCORE},
+   {"\x1F", ILLEGAL},
 }
 
-//Test that basic token parsing works...
+// Test that basic token parsing works...
 func TestScan(t *testing.T) {
-	for _, expected := range scannerTests {
-		s := NewScanner(strings.NewReader(expected.scanvalue))
-		var tok Token
-		var lit string
-		for tok != EOF {
-			tok, lit = s.Scan()
-			if (tok != EOF && expected.expected != EOF) && tok != expected.expected {
-				//EOF returned each scan, so ignore if it's not what we're testing...
-				t.Errorf("FAIL: Got %d '%s' when %d '%s' was expected.", tok, lit, expected.expected, expected.scanvalue)
-			}
-		}
-	}
+   for _, expected := range scannerTests {
+      s := NewScanner(strings.NewReader(expected.scanvalue))
+      var tok Token
+      var lit string
+      for tok != EOF {
+         tok, lit = s.Scan()
+         if (tok != EOF && expected.expected != EOF) && tok != expected.expected {
+            //EOF returned each scan, so ignore if it's not what we're testing...
+            t.Errorf("FAIL: Got %d '%s' when %d '%s' was expected.", tok, lit, expected.expected, expected.scanvalue)
+         }
+      }
+   }
 }
 
-//Test a range of literals that should be allowed by the library
+// Test a range of literals that should be allowed by the library
 func TestValidLiterals(t *testing.T) {
-	var lits = []string{"āēīōūĀĒĪŌŪ", ">>>", "/)(*&^%$#@!>:!@#", "abc123", "子：？"}
-	for _, s := range lits {
-		s := NewScanner(strings.NewReader(s))
-		var tok Token
-		var lit string
-		for tok != EOF {
-			tok, lit = s.Scan()
-			if tok == ILLEGAL {
-				t.Errorf("FAIL: Illegal token '%d' '%s' when expecting valid LITERAL value.", tok, lit)
-			}
-		}
-	}
+   var lits = []string{"āēīōūĀĒĪŌŪ", ">>>", "/)(*&^%$#@!>:!@#", "abc123", "子：？"}
+   for _, s := range lits {
+      s := NewScanner(strings.NewReader(s))
+      var tok Token
+      var lit string
+      for tok != EOF {
+         tok, lit = s.Scan()
+         if tok == ILLEGAL {
+            t.Errorf("FAIL: Illegal token '%d' '%s' when expecting valid LITERAL value.", tok, lit)
+         }
+      }
+   }
 }
--- kval-token.go
+++ kval-token.go
@@ -3,52 +3,46 @@ package kvalscanner
 // Token represents a lexical token.
 type Token int
 
-//Tokens to scan for that satisfy the KVAL (Key Value Access Language) specification
+// Tokens to scan for that satisfy the KVAL (Key Value Access Language) specification
 const (
-	// Special tokens
-	ILLEGAL Token = iota // ILLEGAL: Token is an illegal character
-	EOF                  // EOF: Token signals the end of input
-	WS                   // WS: Token signals whitespace has been found
+   ILLEGAL Token = iota // Spacial token, ILLEGAL: Token is an illegal character
+   EOF                  // Spacial token, EOF: Token signals the end of input
+   WS                   // Spacial token, WS: Token signals whitespace has been found
 
-	// Literals
-	LITERAL // LITERAL: String literal discovered
+   LITERAL // LITERAL: String literal discovered
 
-	// Other Operators (in order)
-	BUCKEY // BUCKKEY: >>>> Bucket to Key syntax for KVAL
-	BUCBUC // BUCBUC: >> Bucket to Bucket syntax for KVAL
-	KEYVAL // KEYVALL :: Key to Value syntax for KVAL
-	ASSIGN // ASSIGN: => Assignment operator for KVAL renames
+   BUCKEY // Other operator, BUCKKEY: >>>> Bucket to Key syntax for KVAL
+   BUCBUC // Other operator, BUCBUC: >> Bucket to Bucket syntax for KVAL
+   KEYVAL // Other operator, KEYVALL :: Key to Value syntax for KVAL
+   ASSIGN // Other operator, ASSIGN: => Assignment operator for KVAL renames
 
-	// Single character operators
-	USCORE // USCORE: _ Return unknown Key or Value
-	OPATT  // OPATT: { Open a regular expression pattern
-	CPATT  // COATT: } Close a regular expression pattern
+   USCORE // Single character operator, USCORE: _ Return unknown Key or Value
+   OPATT  // Single character operator, OPATT: { Open a regular expression pattern
+   CPATT  // Single character operator, COATT: } Close a regular expression pattern
 
-	// Keywords
-	INS // INS: Insert capability of KVAL
-	GET // GET: Get capability of KVAL
-	LIS // LIS: LIS capability of KVAL
-	DEL // DEL: Delete capability of KVAL
-	REN // REN: Rename capability of KVAL
+   INS // Keyword, INS: Insert capability of KVAL
+   GET // Keyword, GET: Get capability of KVAL
+   LIS // Keyword, LIS: LIS capability of KVAL
+   DEL // Keyword, DEL: Delete capability of KVAL
+   REN // Keyword, REN: Rename capability of KVAL
 
-	// Regex
-	REGEX // REGEX: {PATT} ANy regex pattern inside OPATT and CPATT
+   REGEX // Regular expression, REGEX: {PATT} ANy regex pattern inside OPATT and CPATT
 )
 
-//Mapped values aiding in validation of KVAL strings
+// Mapped values aiding in validation of KVAL strings
 var KeywordMap = map[string]int{
-	"INS": 0x1, // Insert
-	"ins": 0x1,
+   "INS": 0x1, // Insert
+   "ins": 0x1,
 
-	"GET": 0x2, // Get values
-	"get": 0x2,
+   "GET": 0x2, // Get values
+   "get": 0x2,
 
-	"LIS": 0x3, // Check existence
-	"lis": 0x3,
+   "LIS": 0x3, // Check existence
+   "lis": 0x3,
 
-	"DEL": 0x4, // Delete values
-	"del": 0x4,
+   "DEL": 0x4, // Delete values
+   "del": 0x4,
 
-	"REN": 0x5, // Rename values
-	"ren": 0x5,
+   "REN": 0x5, // Rename values
+   "ren": 0x5,
 }
